febc7100050614638dc00a40ccfe3d241650dca8
Mayuri1232 <duchalmayuri7@gmail.com>
2025-04-24 09:54:16 +0530

    Add files via upload

---
+++ set.py
@@ -0,0 +1,16 @@
+# Create an empty set
+my_set = set()
+
+# Add members to the set
+my_set.add(10)
+my_set.add(20)
+my_set.add(30)
+
+# Print the set after adding items
+print("Set after adding items:", my_set)
+
+# Remove one item from the set
+my_set.remove(20)
+
+# Print the set after removing an item
+print("Set after removing an item:", my_set)
